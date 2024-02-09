e871aeaa40a0ff67db7d8673dcce47d3d407861c
user <ulanskii03@gmail.com>
2024-02-09 17:52:33 +0600

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        cat.age = 2;
+        cat.name = "rf";
+        System.out.println(cat.name + " " + cat.age);
+        
+    }
+}
