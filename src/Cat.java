fc23d7b26d63251d8315f953b557883789989cb6
user <ulanskii03@gmail.com>
2024-02-09 18:44:08 +0600

    first commit

--- src/Cat.java
+++
@@ -1,5 +0,0 @@
-public class Cat {
-    int age;
-    String name;
-    String weight;
-}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        Cat cat = new Cat();
-        cat.age = 2;
-        cat.name = "rf";
-        System.out.println(cat.name + " " + cat.age);
 
+        System.out.println("first task");
     }
 }
